1b833532eec4558b179394e95ffde04752071c49
PrasannaJung <prasannajung08@gmail.com>
2022-11-08 16:49:08 +0545

    Styling added

--- expense-tracker/src/components/ExpensesList.tsx
+++ expense-tracker/src/components/ExpensesList.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { ExpenseData } from "../App";
+import styles from "./ExpenseList.module.css";
 
 type Props = {
   list: ExpenseData[];
 };
 
 const ExpensesList = (props: Props) => {
+  if (props.list.length === 0) {
+    return <div></div>;
+  }
   return (
-    <div>
+    <div className={styles.list}>
       <ul>
         {props.list.map(item => {
           return (
             <li>
-              <div>{item.price}</div>
-              <div>{item.text}</div>
-              <div>{item.date}</div>
+              <div className={styles.date}>{item.date}</div>
+              <div className={styles.itemName}>{item.text}</div>
+              <div className={styles.price}>{item.price}</div>
             </li>
           );
         })}
